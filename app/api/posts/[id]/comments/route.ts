34abd6b7f4ac7598d84412df1fbd8909938b1413
Mohammed Gharbi <mohammedgharbi@icloud.com>
2025-09-08 10:48:30 +0100

    fix: get arguments

--- app/api/posts/[id]/comments/route.ts
+++ app/api/posts/[id]/comments/route.ts
@@ -1,11 +1,12 @@
 import { prisma } from "@/lib/prisma"
 import { NextResponse } from "next/server"
 
-export async function GET(req: Request, context: { params: { id: string } }) {
-  const { id } = context.params
-
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
   const comments = await prisma.comment.findMany({
-    where: { postId: id },
+    where: { postId: params.id },
     include: { author: true },
     orderBy: { createdAt: "desc" },
   })
